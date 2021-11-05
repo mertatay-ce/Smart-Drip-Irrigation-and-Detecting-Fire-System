6110f82fa050465baab1848d6e39326bf328fa38
MelihYesilyurt <180202060@kocaeli.edu.tr>
2021-11-05 18:51:57 +0300

    Project created

---
+++ Smart-Drip-Irrigation-and-Detecting-Fire-System/app/src/main/java/com/example/smart_drip_irrigation_and_detecting_fire_system/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.smart_drip_irrigation_and_detecting_fire_system;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
